3634cc84350decf1f360a55ee728c8b64446adf9
unknown <joks026@gmail.com>
2021-10-02 23:21:48 +0800

    vue router added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <div class="text-3xl text-red-500">This is our home   page</div>
+    <div class="flex bg-gray-100 border-b border-gray-300 py-4">
+        <div class="container mx-auto">
+                <router-link class="mr-4" to=''>Home</router-link>
+                <router-link to='/about'>About</router-link>
+        </div>
+    </div>
+    <div class="container mx-auto">
+        <router-view></router-view>
+    </div>
+    
 @endsection
