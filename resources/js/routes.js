4c56c004f14df0f16c65e3c8a5e06ccbc238b853
unknown <joks026@gmail.com>
2021-10-03 03:20:56 +0800

    pages added

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,16 @@
 import Home from './components/Home';
 import About from './components/About';
-
+import Notfound from './components/Notfound';
 
 export default{
     mode: 'history',
+    linkActiveClass:'font-semibold',
 
     routes:[
+        {
+            path: '*',
+            component: Notfound
+        },
         {
             path:'/',
             component: Home
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="flex bg-gray-100 border-b border-gray-300 py-4">
         <div class="container mx-auto">
-                <router-link class="mr-4" to=''>Home</router-link>
+                <router-link class="mr-4" to='/' exact>Home</router-link>
                 <router-link to='/about'>About</router-link>
         </div>
     </div>
